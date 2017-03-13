2d7548e3ae6108f69d2085cf769a5b9b67845d73
Ankit Rai <raiankit953@gmail.com>
2017-03-13 23:37:39 +0530

    fully functioning authentication-via-google environment

--- index.php
+++ index.php
@@ -2,44 +2,8 @@
 //Include GP config file && User class
 include_once 'google/gpConfig.php';
 include_once 'google/User.php';
-
-if(isset($_GET['code'])){
-	$gClient->authenticate($_GET['code']);
-	$_SESSION['token'] = $gClient->getAccessToken();
-	header('Location: ' . filter_var($redirectURL, FILTER_SANITIZE_URL));
-}
-
-if (isset($_SESSION['token'])) {
-	$gClient->setAccessToken($_SESSION['token']);
-}
-
-if ($gClient->getAccessToken()) {
-	//Get user profile data from google
-	$gpUserProfile = $google_oauthV2->userinfo->get();
-	
-	//Initialize User class
-	$user = new User();
-	
-	//Insert or update user data to the database
-    $gpUserData = array(
-        'oauth_provider'=> 'google',
-        'oauth_uid'     => $gpUserProfile['id'],
-        'first_name'    => $gpUserProfile['given_name'],
-        'last_name'     => $gpUserProfile['family_name'],
-        'email'         => $gpUserProfile['email'],
-        'gender'        => $gpUserProfile['gender'],
-        'locale'        => $gpUserProfile['locale'],
-        'picture'       => $gpUserProfile['picture'],
-        'link'          => $gpUserProfile['link']
-    );
-    $userData = $user->checkUser($gpUserData);
-	
-	//Storing user data into session
-	$_SESSION['userData'] = $userData;
-} else {
-	$authUrl = $gClient->createAuthUrl();
-	$output = filter_var($authUrl, FILTER_SANITIZE_URL);
-}
+$authUrl = $gClient->createAuthUrl();
+$output = filter_var($authUrl, FILTER_SANITIZE_URL);
 ?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
