c61350dc09fcfc54fcc6245fb5904b4d5de7857b
Ankit Rai <raiankit953@gmail.com>
2017-03-13 21:17:23 +0530

    trial 1: google login

---
+++ google/gpConfig.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+//Include Google client library 
+include_once 'src/Google_Client.php';
+include_once 'src/contrib/Google_Oauth2Service.php';
+
+/*
+ * Configuration and setup Google API
+ */
+$clientId = 'InsertGoogleAppClientID'; //Google client ID
+$clientSecret = 'InsertGoogleAppClientSecret'; //Google client secret
+$redirectURL = 'http://localhost/login_with_google_using_php/'; //Callback URL
+
+//Call Google API
+$gClient = new Google_Client();
+$gClient->setApplicationName('Login to CodexWorld.com');
+$gClient->setClientId($clientId);
+$gClient->setClientSecret($clientSecret);
+$gClient->setRedirectUri($redirectURL);
+
+$google_oauthV2 = new Google_Oauth2Service($gClient);
+?>
