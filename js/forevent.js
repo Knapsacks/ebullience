108bc5d076878769519b9b3d95619f913d77ec0b
Ankit Rai <ankitrai96@users.noreply.github.com>
2017-03-21 08:46:19 +0530

    Update forevent.js

--- js/forevent.js
+++ js/forevent.js
@@ -141,9 +141,9 @@ $(document).ready(function(){
             });
             setTimeout(load_event_page, 1000);
             function load_event_page(){
-                $('#user_image').fadeIn('slow', function(){
-			$('#feedback_button').fadeIn('slow');
-			$('#logout_button').fadeIn('slow');
+		$('#feedback_button').fadeIn('slow');
+		$('#logout_button').fadeIn('slow');
+                $('#user_image').fadeIn('slow', function(){	
                     if($(window).width()>=900){
                             $('#Profile').css('transform','translate(-290%,-150%)');
                             $('#profile_description').css('opacity','1');
